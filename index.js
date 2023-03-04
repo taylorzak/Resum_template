96731d46b3db27a23178c6f1a37d909aac02cfb4
Zak Taylor~ <taylorzak@hotmail.com>
2023-03-04 15:19:26 -0700

    first push

---
+++ index.js
@@ -0,0 +1,10 @@
+
+$(window).resize(function() { 
+  
+    if ($(window).width() > 928) {
+      $('aside').height($('section').height())
+    } else {
+      $('aside').height('auto')
+    }
+     
+  });
